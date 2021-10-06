561b5f688284d1a614fa59d6f115f7b2545cbcc1
ujjwal270700c <ujjwal270700c@gmail.com>
2021-10-06 13:52:16 +0530

    set up the server and connect mongodb

---
+++ server.js
@@ -0,0 +1,37 @@
+const express=require('express');
+require('dotenv').config()
+const cors=require('cors');
+const logger=require('morgan');
+const mongoose = require('mongoose');
+
+const app=express();
+const port =process.env.PORT;
+
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+app.use(cors());
+
+// Connect to Database
+
+mongoose.connect(process.env.API,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+  
+}).then(() => console.log('DB Connection Successful'))
+.catch((err) =>{
+    console.log('Database Connection failed....');
+    console.log(err);
+})
+
+// Routes
+
+app.get('/',(req,res)=>{
+    res.send('Welcome');
+})
+
+// Event listener for http server 
+app.listen(port,()=>{
+    console.log(`Server is running at port :${port}`);
+})
